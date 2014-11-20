b313b6c66b0ad2c2e02c8cb979cdc47e86977d6c
Johnny Hall <johnny@recipher.co.uk>
2014-11-20 09:30:50 +0000

    Fix to ensure initializer is fired before first page loads

--- app-addon/initializers/i18next.js
+++ app-addon/initializers/i18next.js
@@ -6,7 +6,9 @@ import helper from '../helpers/i18next';
 export default {
   name: 'i18next'
 
-, initialize: function(container) {
+, initialize: function(container, app) {
+    app.deferReadiness();
+
     var locale = localStorage.locale || 'en';
 
     i18n.init({
@@ -28,6 +30,8 @@ export default {
       Ember.A([ 'controller', 'view', 'route' ]).forEach(function(component) {
         container.injection(component, 'i18n', 'app:i18n');
       });
+
+      app.advanceReadiness();
     });
 
     i18n.addPostProcessor('capitalize', function(value) {
